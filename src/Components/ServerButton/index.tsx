6db3183648d3966bf53f0a90afe877d33e49cb7b
Xavi002 <diogoxavier14@hotmail.com>
2020-06-19 15:05:50 +0100

    add server-list

--- src/Components/ServerButton/index.tsx
+++ src/Components/ServerButton/index.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
+import Logo from '../../assets/Logo.svg';
+
 import {Button} from './styles';
 
-interface Props {
+export interface Props {
   selected?: boolean;
   isHome?: boolean;
   hasNotifications?: boolean;
   mentions?: number;
 }
 
-const ServerButton. React.FC = () => {};
+const ServerButton: React.FC<Props> = ({ 
+  selected,
+  isHome,
+  hasNotifications,
+  mentions
+}) => {
+  return (
+    <Button
+      isHome={isHome}
+      hasNotifications={hasNotifications}
+      mentions={mentions}
+      className={selected ? 'active' : ''}
+    >
+      {isHome && <img src={Logo} alt="Discord" />}
+    </Button>
+  );
+};
 
 export default ServerButton;
--- src/Components/ServerList/index.tsx
+++ src/Components/ServerList/index.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
+import ServerButton from '../ServerButton';
+
 import {Container, Separator} from './styles';
 
 const ServerList: React.FC = () => {
   return (
     <Container>
-      <SeverButton isHome />
+      <ServerButton isHome />
 
       <Separator />
 
-      <SeverButton />
-      <SeverButton hasNotifications />
-      <SeverButton mentions={3} />
-      <SeverButton />
-      <SeverButton />
-      <SeverButton />
-      <SeverButton />
-      <SeverButton mentions={23} />
-      <SeverButton />
-      <SeverButton />
-      <SeverButton hasNotifications />
-      <SeverButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={3} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton mentions={23} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
 
     </Container>
   );
