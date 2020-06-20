5fafec3414e7539b48aca809a7186c8c5da1fcd1
Xavi002 <diogoxavier14@hotmail.com>
2020-06-20 19:20:32 +0100

    add user-list

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -7,6 +7,7 @@ import ServerName from '../ServerName';
 import ChannelInfo from '../ChannelInfo';
 import ChannelList from '../ChannelList';
 import UserInfo from '../UserInfo';
+import UserList from '../UserList';
 
 const Layout: React.FC = () => {
   return <Grid>
@@ -15,6 +16,9 @@ const Layout: React.FC = () => {
     <ChannelInfo />
     <ChannelList />
     <UserInfo />
+    <div />
+    <UserList />
+    
   </Grid>
 
 };
---
+++ src/Components/UserList/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {Container, Role, User, Avatar} from './styles';
+
+interface UserProps {
+  nickname: string;
+  isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({nickname, isBot}) => {
+  return (
+    <User>
+      <Avatar className={isBot ? 'bot' : ''} />
+
+      <strong>{nickname}</strong>
+
+      {isBot && <span>Bot</span>}
+    </User>
+  );
+}
+
+const UserList: React.FC = () => {
+  return (
+    <Container>
+      <Role>Disponivel - 1</Role>
+      <UserRow nickname="XavI" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+
+      <Role>Offline - 18</Role>
+      <UserRow nickname="Music" isBot />
+      <UserRow nickname="Person" isBot />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+      <UserRow nickname="Person" />
+    </Container>
+  );
+};
+
+export default UserList;
