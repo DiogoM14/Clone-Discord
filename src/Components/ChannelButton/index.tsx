8caa416656e5598158602fda3b7cf1532fc6703e
Xavi002 <diogoxavier14@hotmail.com>
2020-06-20 14:12:27 +0100

    add channel-list

---
+++ src/Components/ChannelButton/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {Container, HashtagIcon, InviteIcon, SettingsIcon} from './styles';
+
+export interface Props {
+  channelName: string;
+  selected?: boolean;
+}
+
+const ChannelButton: React.FC<Props> = ({ channelName, selected }) => {
+  return (
+    <Container className={selected ? 'active' : ''}>
+      <div>
+        <HashtagIcon />
+        <span>{channelName}</span>
+      </div>
+
+      <div>
+        <InviteIcon className={'icon'} />
+        <SettingsIcon className={'icon'} />
+      </div>
+    </Container>
+  );
+};
+
+export default ChannelButton;
